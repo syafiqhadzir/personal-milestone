7928d1f47ccf6a2ac57c0528dfa6f709ab3ddc36
syafiqhadzir <syafiqhadzir@live.com.my>
2023-03-01 14:57:42 +0800

    MVC Controllers

--- csharp-tutorials/unit-testing-csharp/RunGroop/RunGroopWebApp.Tests/Controller/ClubControllerTests.cs
+++ csharp-tutorials/unit-testing-csharp/RunGroop/RunGroopWebApp.Tests/Controller/ClubControllerTests.cs
@@ -20,37 +20,37 @@ namespace RunGroopWebApp.Tests.Controller
 
         public ClubControllerTests()
         {
-            //Dependencies
+            // Dependencies
             _clubRepository = A.Fake<IClubRepository>();
             _photoService = A.Fake<IPhotoService>();
             _httpContextAccessor = A.Fake<HttpContextAccessor>();
 
-            //SUT
+            // SUT
             _clubController = new ClubController(_clubRepository, _photoService);
         }
 
         [Fact]
         public void ClubController_Index_ReturnsSuccess()
         {
-            //Arrange - What do i need to bring in?
+            // Arrange - What do i need to bring in?
             var clubs = A.Fake<IEnumerable<Club>>();
             A.CallTo(() => _clubRepository.GetAll()).Returns(clubs);
-            //Act
+            // Act
             var result = _clubController.Index();
-            //Assert - Object check actions
+            // Assert - Object check actions
             result.Should().BeOfType<Task<IActionResult>>();
         }
 
         [Fact]
         public void ClubController_Detail_ReturnsSuccess()
         {
-            //Arrange
+            // Arrange
             var id = 1;
             var club = A.Fake<Club>();
             A.CallTo(() => _clubRepository.GetByIdAsync(id)).Returns(club);
-            //Act
+            // Act
             var result = _clubController.DetailClub(id, "RunningClub");
-            //Assert
+            // Assert
             result.Should().BeOfType<Task<IActionResult>>();
         }
     }
